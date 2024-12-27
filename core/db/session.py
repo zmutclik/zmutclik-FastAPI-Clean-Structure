af248c94778af605206ef593e111cfd01d580c01
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2024-12-27 16:07:21 +0700

    -

--- core/db/session.py
+++ core/db/session.py
@@ -42,28 +42,3 @@ session: Union[AsyncSession, async_scoped_session] = async_scoped_session(
     session_factory=async_session,
     scopefunc=get_session_id,
 )
-# class DBSessionWrapper:
-#     def __init__(self, session_factory):
-#         self._session_factory = session_factory
-
-#     def __await__(self):
-#         return self._get_session().__await__()
-
-#     @asynccontextmanager
-#     async def _generate_session(self):
-#         async with self._session_factory() as session:
-#             try:
-#                 yield session
-#             finally:
-#                 await session.close()
-
-#     async def _get_session(self):
-#         async with self._generate_session() as session:
-#             return session
-
-#     async def __call__(self):
-#         return await self._get_session()
-
-
-# session: Generator[AsyncSession, None, None] = DBSessionWrapper(async_session)
-
