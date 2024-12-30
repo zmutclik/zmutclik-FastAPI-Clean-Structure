7f3d62a67a0696b8e0668d715a91b8abfdae930d
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2024-12-30 11:18:32 +0700

    -

--- app/_sys/sysrepo/exceptions/sysrepo.py
+++ app/_sys/sysrepo/exceptions/sysrepo.py
@@ -4,7 +4,7 @@ from core.exceptions import CustomException
 class SysRepoDuplicateException(CustomException):
     code = 400
     error_code = 10200
-    message = "duplicate name at this allocation is use "
+    message = "duplicate sysrepo name at this allocation is use "
     
     
 class SysRepoNotFoundException(CustomException):
