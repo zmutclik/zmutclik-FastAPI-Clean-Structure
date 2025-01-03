e2f35e1ee1e116015b244ca1890f39bdd53282d5
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2025-01-03 11:07:50 +0700

    -

--- pages/base.py
+++ pages/base.py
@@ -1,9 +1,10 @@
 from fastapi.templating import Jinja2Templates
 from core import config
 
+
+def global_context():
+    return {"app_name": config.APP_NAME}
+
+
 templates = Jinja2Templates(directory="pages/templates")
-templates.env.globals.update(
-    add_global_context={
-        "app_name": config.APP_NAME,
-    }
-)
+templates.env.globals.update(global_context=global_context)
